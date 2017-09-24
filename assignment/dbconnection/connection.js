a3f5c9f258adcf5ea63fbde074ab31a9a532dffc
Sanjay Murali <sanjaymurali@ccs.neu.edu>
2017-09-23 20:29:03 -0400

    Project

--- assignment/dbconnection/connection.js
+++ assignment/dbconnection/connection.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
 
 
     if (process.env.MLAB_USERNAME) {
-        connectionString = process.env.MLAB_USERNAME + ":" +
+        connectionString = "mongodb://"process.env.MLAB_USERNAME + ":" +
             process.env.MLAB_PASSWORD + "@" +
             process.env.MLAB_HOST + ':' +
             process.env.MLAB_PORT + '/' +
